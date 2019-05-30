d87aa6f53fd47ac08399cab9d7e8ee3cbc2e701d
Makc <kalyamaly26@mail.ru>
2019-05-30 22:53:36 +0300

    это наш первый комит

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+//alert("Hello world!");
+// let answer = confirm("Are you here");
+//console.log(answer);
+//console.log(4 + " - object");
+/*let incr = 10,
+decr = 10;
+  
+  console.log(incr++);
+  console.log(decr--);
+  console.log(5%2);
+  console.log("2"=== 2);
+  let isChecked = true,
+  isClose = false;
+
+  console.log(isChecked || isClose);
+
+  let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+  };
+  
+  console.log(persone["name"]);*/
+
+  //let arr =['plum.png', 'orange.jpg','apple.bmp'];
+
+  //console.log(arr[0]);
+
+  //alert('Hello world!');
+
+  //let answer = confirm('are you here?');
+  //console.log(answer);
+  //let arr =['plum.png', 'orange.jpg','apple.bmp'];
+  //let answer = +prompt('are you here?', 'yes');
+  
+let incr = 10,
+decr =10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log('2'=== 2);
+
+let isChecked = false,
+isClose = false;
+console.log(isChecked || !isClose);
